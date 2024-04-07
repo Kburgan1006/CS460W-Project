d637812aa1f23890c7acddceba1288ba015634bb
wpit63 <73858845+wpit63@users.noreply.github.com>
2024-04-07 19:25:49 -0400

    Update setup.sql
    fixed errors

--- Code/MySQL Database/setup.sql
+++ Code/MySQL Database/setup.sql
@@ -2,8 +2,8 @@ DROP DATABASE IF EXISTS medical_database;
 
 CREATE DATABASE medical_database;
 
-DROP USER 'myuser'@'localhost' IDENTIFIED BY 'mypass';
-DROP USER 'remuser'@'%' IDENTIFIED BY 'rempass';
+DROP USER 'myuser'@'localhost';
+DROP USER 'remuser'@'%';
 
 CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mypass';
 CREATE USER 'remuser'@'%' IDENTIFIED BY 'rempass';
@@ -12,7 +12,7 @@ GRANT ALL ON *.* TO 'myuser'@'localhost';
 GRANT ALL ON *.* TO 'remuser'@'%';
 FLUSH PRIVILEGES;
 
-USE medical_database;  
+USE medical_database;
 
 CREATE TABLE patient_table (
     patient_fname CHAR(20) NOT NULL,
@@ -22,19 +22,19 @@ CREATE TABLE patient_table (
     patient_email VARCHAR(40) NOT NULL,
     patient_DOB TIMESTAMP NOT NULL,
     patient_gender CHAR(20) NOT NULL,
-    patient_occupation CHAR(25) NOT NULL, /*not needed?*/
-    patient_medHist VARCHAR(MAX),
-    ec_name CHAR(20),
-    ec_phone VARCHAR(15),
-    ec_relation CHAR(15),
-    comment VARCHAR(MAX),
+    patient_occupation CHAR(25) NOT NULL,
+    patient_medHist VARCHAR(8000) NOT NULL,
+    ec_name CHAR(20) NOT NULL,
+    ec_phone VARCHAR(15) NOT NULL,
+    ec_relation CHAR(15) NOT NULL,
+    comment VARCHAR(8000) NOT NULL,
     patient_id INT(5) NOT NULL AUTO_INCREMENT,
     admit_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     discharge_time TIMESTAMP NOT NULL,
     patient_illness CHAR(40) NOT NULL,
-    patient_medication CHAR(40) NOT NULL
+    patient_medication CHAR(40) NOT NULL,
 
-    FOREIGN KEY (patient_id) REFERENCES admission_table(patient_id) ON DELETE CASCADE
+    PRIMARY KEY (patient_id)
 );
 
 CREATE TABLE testing_table (
@@ -42,7 +42,7 @@ CREATE TABLE testing_table (
     result CHAR(60) NOT NULL,
     order_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (patient_id) REFERENCES admission_table(patient_id) ON DELETE CASCADE
+    FOREIGN KEY (patient_id) REFERENCES patient_table(patient_id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee_table (
