228b81cee0dc7e9a0d6201a94a8e23e62fec0f37
wpit63 <73858845+wpit63@users.noreply.github.com>
2024-04-07 16:20:00 -0400

    Update setup.sql
    flattened patient and admission tables, added new rows to reflect forms

--- Code/MySQL Database/setup.sql
+++ Code/MySQL Database/setup.sql
@@ -12,23 +12,27 @@ GRANT ALL ON *.* TO 'myuser'@'localhost';
 GRANT ALL ON *.* TO 'remuser'@'%';
 FLUSH PRIVILEGES;
 
-USE medical_database;
-
-CREATE TABLE admission_table (
-    patient_id INT(5) NOT NULL AUTO_INCREMENT,
-    admit_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    discharge_time TIMESTAMP NOT NULL,
-
-    PRIMARY KEY (patient_id)
-);
-    
+USE medical_database;  
 
 CREATE TABLE patient_table (
     patient_fname CHAR(20) NOT NULL,
     patient_lname CHAR(20) NOT NULL,
+    patient_address VARCHAR(80) NOT NULL,
+    patient_phoneNum VARCHAR(15) NOT NULL,
+    patient_email VARCHAR(40) NOT NULL,
+    patient_DOB TIMESTAMP NOT NULL,
+    patient_gender CHAR(20) NOT NULL,
+    patient_occupation CHAR(25) NOT NULL, /*not needed?*/
+    patient_medHist VARCHAR(MAX),
+    ec_name CHAR(20),
+    ec_phone VARCHAR(15),
+    ec_relation CHAR(15),
+    comment VARCHAR(MAX),
+    patient_id INT(5) NOT NULL AUTO_INCREMENT,
+    admit_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    discharge_time TIMESTAMP NOT NULL,
     patient_illness CHAR(40) NOT NULL,
-    patient_medication CHAR(40) NOT NULL,
-    patient_id INT(5) NOT NULL,
+    patient_medication CHAR(40) NOT NULL
 
     FOREIGN KEY (patient_id) REFERENCES admission_table(patient_id) ON DELETE CASCADE
 );
